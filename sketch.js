d140c43b4865c28387b14031b4b5a3a2d2abac10
zadacherian <zada.cherian@yahoo.com>
2019-12-15 16:30:48 +0530

    hi

--- sketch.js
+++ sketch.js
@@ -34,7 +34,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+platform = new Ground(180,305,300,170);
 }
 
 function draw(){
@@ -59,4 +59,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
